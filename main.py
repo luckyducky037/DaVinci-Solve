8d5536704a9548b44fe70ca7d7d59ad528847f59
luckyducky037 <leonardozhou2020@gmail.com>
2024-09-14 08:30:32 -0400

    Added listen, response

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import speech_recognition as sr
 import pvorca
 from groq import Groq
 from dotenv import load_dotenv
@@ -7,3 +8,46 @@ load_dotenv()
 
 groq_client = Groq(api_key=getenv("GROQ"))
 orca_client = pvorca.create(access_key=getenv("ORCA"))
+
+def record_until_silence():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Adjusting for ambient noise. Please wait...")
+        r.adjust_for_ambient_noise(source, duration=1)
+        print("Speak now. Recording will stop after silence is detected.")
+
+        audio = r.listen(source, phrase_time_limit=None, timeout=None)
+
+    return audio
+
+def listen(filename):
+    audio = record_until_silence()
+    print(audio)
+
+    wav_data = audio.get_wav_data()
+
+    with open(f"{filename}.wav", "wb") as wav_file:
+        wav_file.write(wav_data)
+
+    print(f"WAV file saved as '{filename}.wav'")
+
+    return filename
+
+def groq_listen():
+    pass
+
+def groq_response(prompt):
+    chat_completion = groq_client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        model="llama3-8b-8192",
+    )
+
+    print(chat_completion.choices[0].message.content)
+
+def orca_speak(text):
+    pass
